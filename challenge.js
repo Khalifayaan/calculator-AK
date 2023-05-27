07798637b7970dec37449345242bee617754826c
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-05-27 21:05:25 +0300

    Claculator assignment - Done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,23 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+function calculator(num1, num2, operator) {
+    if (operator === "+") {
+      console.log(num1 + num2);
+    } else if (operator === "-") {
+      console.log(num1 - num2);
+    } else if (operator === "*") {
+      console.log(num1 * num2);
+    } else if (operator === "/") {
+      console.log(num1 / num2);
+    } else {
+      console.log("Invalid operator");
+    }
+  }
+
+  calculator(8, 2, "+")
+  calculator(7, 3, "-")
+  calculator(3, 3, "*")
+  calculator(8, 2, "/")
+ 
